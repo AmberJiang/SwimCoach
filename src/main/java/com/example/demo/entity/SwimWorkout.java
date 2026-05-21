4b7cd7fc66b4bfd46b05faa5876adccf1a7b20ac
Nanyan <ambbeerjiang@gmail.com>
2026-05-21 09:16:27 +0800

    chore:import

--- src/main/java/com/example/demo/entity/SwimWorkout.java
+++ src/main/java/com/example/demo/entity/SwimWorkout.java
@@ -1,5 +1,10 @@
 package com.example.demo.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import org.springframework.data.annotation.Id;
+
 import java.time.LocalDateTime;
 
 @Entity
